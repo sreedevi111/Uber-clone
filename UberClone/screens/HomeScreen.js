96c74e848ee36bd28198bb71e3761120d100dcae
Sreedevi S <sreedevisobhana@gmail.com>
2022-05-07 00:41:49 +0530

    added uber logo

--- UberClone/screens/HomeScreen.js
+++ UberClone/screens/HomeScreen.js
@@ -1,6 +1,6 @@
 //rnfes 
 
-import { StyleSheet, Text, View, } from 'react-native'
+import { StyleSheet, Text, View, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
@@ -9,7 +9,19 @@ const HomeScreen = () => {
   return (
       //safeareaview prevent in going content above notification bar 
     <SafeAreaView style = {tw`bg-white h-full`}>
-      <Text style = {tw`text-blue-500 p-10`}>I am home screen</Text>
+     <View style= {tw`p-5`}>
+        <Image 
+            style={{
+                width: 100,
+                 height: 100, 
+                 resizeMode:'contain',
+            }}
+            source={{
+                uri: "https://links.papareact.com/gzs"
+                //uniform resource identifier similar to url
+            }}
+        />
+     </View>
     </SafeAreaView>
   )
 }
