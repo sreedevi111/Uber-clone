958d040eb4e3cdfd0b4e074b43660d6623c4cb48
Sreedevi S <sreedevisobhana@gmail.com>
2022-05-06 23:16:04 +0530

    setup redux

--- UberClone/App.js
+++ UberClone/App.js
@@ -1,14 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 //setup redux
 
 export default function App() {
   return (
+    //Provider will make store available to each component
+    <Provider store={store}>   
     <View style={styles.container}>
       <Text>Lets build Uber!</Text>
-      
-    </View>
+        </View>
+    </Provider>
   );
 }
 
